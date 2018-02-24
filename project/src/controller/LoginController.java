696de62a7dc9c863e85729e4eabf15988ebe2129
Onurcem KARAKOC <1150606806@nku.edu.tr>
2018-02-24 12:14:09 +0300

    commit

--- project/src/controller/LoginController.java
+++ project/src/controller/LoginController.java
@@ -1,11 +1,14 @@
 package controller;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
@@ -15,43 +18,49 @@ import javafx.stage.Stage;
 public class LoginController implements Initializable {
 	private Scene signUpscene;
 
-	public void setSignUpScene(Scene signUpScene) {
-		signUpscene=signUpScene;
+	public void setSignUpScene() throws IOException {
+		Parent root = FXMLLoader.load(getClass().getResource("/fxml/signup.fxml"));
+		signUpscene = new Scene(root);
 	}
-	
+
 	@FXML
 	private Button login;
-	
+
 	@FXML
 	private Button sign_up;
-	
+
 	@FXML
 	private Button forget_password;
 
 	@FXML
 	private TextField user_email;
-	
+
 	@FXML
 	private PasswordField user_password;
 
 	public void initialize(URL location, ResourceBundle resources) {
-
+		try {
+			setSignUpScene();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
+
 	public void hi(ActionEvent actionEvent) {
-		if(actionEvent.getSource().equals(login)) {
+		if (actionEvent.getSource().equals(login)) {
 			if (user_email.getText().isEmpty() || user_password.getText().isEmpty()) {
 				System.out.println("düzgün veri girin");
 			} else {
-				Stage stage=(Stage) login.getScene().getWindow();
+				Stage stage = (Stage) login.getScene().getWindow();
 				stage.setScene(signUpscene);
 			}
-		}else if(actionEvent.getSource().equals(forget_password)) {
+		} else if (actionEvent.getSource().equals(forget_password)) {
 			System.out.println("mal þifre unutulur mu :D");
-		}else {
+		} else {
 			System.out.println("sign");
 		}
-		
-		
+
 	}
 
 }
--- project/src/main/MainClass.java
+++ project/src/main/MainClass.java
@@ -17,19 +17,9 @@ public class MainClass extends Application {
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 
-		FXMLLoader LoginFxmlLoader=new FXMLLoader(getClass().getResource("/fxml/login.fxml"));
-	    Parent root = LoginFxmlLoader.load();
-	    FXMLLoader SignUpFxmlLoader=new FXMLLoader(getClass().getResource("/fxml/signup.fxml"));
-	    Parent root2 =SignUpFxmlLoader.load();
-	    Scene loginScene=new Scene(root);
-	    Scene signUpScene=new Scene(root2);
-	    LoginController loginController = LoginFxmlLoader.getController();
-	    loginController.setSignUpScene(signUpScene);
-	    
-	    
+	    Parent root = new FXMLLoader(getClass().getResource("/fxml/login.fxml")).load();
 	    primaryStage.setResizable(false);
-		//fatma
-        primaryStage.setScene(loginScene);
+        primaryStage.setScene(new Scene(root));
 		primaryStage.show();
 	}
 
